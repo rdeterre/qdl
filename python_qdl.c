5208a0273389f6710017ce4e458b342f04202941
Romain Deterre <rdeterre@ryzentosh.local>
2021-03-17 21:03:42 -0400

    Allow threads - v1.1.1

--- python_qdl.c
+++ python_qdl.c
@@ -20,32 +20,43 @@ static PyObject *qdl_run(PyObject *self, PyObject *args) {
   if (!PyArg_ParseTuple(args, "ssss", &storage, &mbn, &program, &patch))
     return NULL;
 
+  Py_BEGIN_ALLOW_THREADS
   type = detect_type(program);
+  Py_END_ALLOW_THREADS
   if (type != QDL_FILE_PROGRAM) {
     PyErr_Format(PyExc_RuntimeError,
                  "Program passed is not a QDL program. Got type %d", type);
     return NULL;
   }
 
+  Py_BEGIN_ALLOW_THREADS
   ret = program_load(program);
+  Py_END_ALLOW_THREADS
   if (ret < 0) {
     PyErr_Format(PyExc_RuntimeError, "Program load failed. Error %d", ret);
     return NULL;
   }
 
+  Py_BEGIN_ALLOW_THREADS
   type = detect_type(patch);
+  Py_END_ALLOW_THREADS
   if (type != QDL_FILE_PATCH) {
     PyErr_Format(PyExc_RuntimeError,
                  "Patch passed is not a QDL patch. Got type %d", type);
     return NULL;
   }
+
+  Py_BEGIN_ALLOW_THREADS
   ret = patch_load(patch);
+  Py_END_ALLOW_THREADS
   if (ret < 0) {
     PyErr_Format(PyExc_RuntimeError, "Patch load failed. Error %d", ret);
     return NULL;
   }
 
+  Py_BEGIN_ALLOW_THREADS
   libusb_init(NULL);
+  Py_END_ALLOW_THREADS
   ret = find_device(&qdl);
   if (ret) {
     libusb_exit(NULL);
@@ -53,14 +64,18 @@ static PyObject *qdl_run(PyObject *self, PyObject *args) {
     return NULL;
   }
 
+  Py_BEGIN_ALLOW_THREADS
   ret = sahara_run(&qdl, mbn);
+  Py_END_ALLOW_THREADS
   if (ret < 0) {
     libusb_exit(NULL);
     PyErr_Format(PyExc_RuntimeError, "Could not run Sahara. Error %d\n", ret);
     return NULL;
   }
 
+  Py_BEGIN_ALLOW_THREADS
   ret = firehose_run(&qdl, NULL, storage);
+  Py_END_ALLOW_THREADS
   if (ret < 0) {
     libusb_exit(NULL);
     PyErr_Format(PyExc_RuntimeError, "Could not run Firehose. Error %d\n", ret);
